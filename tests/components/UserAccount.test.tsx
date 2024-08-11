a607618a79de4f2803c0b6da86b7d6b162fbe9bc
joshanaba <anabajoshchi@gmail.com>
2024-08-11 17:29:19 +0100

    optimize test code

--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -4,22 +4,25 @@ import UserAccount from '../../src/components/UserAccount'
 import { User } from '../../src/entities'
 
 describe('UserAccount', () => {
+  const renderComponent = (user:User) => {
+    render(<UserAccount user={user} />)
+    return {
+      button: screen.queryByRole('button')
+    }}
   it('should render user name', () => {
     const user:User = {name: 'John', isAdmin: true, id: 1}
-    render(<UserAccount user={user} />)
+    renderComponent(user)
     expect(screen.getByText(user.name)).toBeInTheDocument()
   })
   it('should render edit button if user is admin', () => {
     const user:User = {name: 'John', isAdmin: true, id: 1}
-    render(<UserAccount user={user} />)
-    const button = screen.getByRole('button')
+    const {button} = renderComponent(user)
     expect(button).toBeInTheDocument()
     expect(button).toHaveTextContent(/edit/i)
   })
   it('should not render edit button if user is not admin', () => {
     const user:User = {name: 'John', id: 1}
-    render(<UserAccount user={user} />)
-    const button = screen.queryByRole('button')
+    const {button} = renderComponent(user)
     expect(button).not.toBeInTheDocument()
   })
 })
